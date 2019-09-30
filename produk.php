e2dfa6012e1dbbed0b1545935e7c0d4e400ce8c3
Ichlasul Al <m.ichal7749@mail.unpas.ac.id>
2019-09-30 08:14:56 +0700

    menambahkan instance produk3 dan memasukan nilai serta menampilkannya

--- produk.php
+++ produk.php
@@ -11,8 +11,20 @@ class Produk{
            $harga = 0;
 }
 
-$produk1 = new Produk();
+// $produk1 = new Produk();
+// $produk1->judul = "Black Clover";
+// var_dump ($produk1);
 
-$produk1->judul = "Black Clover";
+// $produk2 = new Produk();
+// $produk2->judul = "Warframe";
+// $produk2->tambahProperty = "test";
+// var_dump ($produk2);
 
-var_dump ($produk1);s
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishinoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
+
+echo "Komik : $produk3->penulis,$produk3->penerbit";
