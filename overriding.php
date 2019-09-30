59faa139072849e0d63e7fe8242184a6a1deb682
Ichlasul Al <m.ichal7749@mail.unpas.ac.id>
2019-09-30 17:55:53 +0700

    membuat overriding.php

---
+++ overriding.php
@@ -0,0 +1,71 @@
+<?php
+
+// jualan produk
+// komik
+// game
+
+class Produk{
+    public $judul,
+           $penulis,
+           $pnerbit,
+           $harga,
+           $jmlHalaman,
+           $waktuMain;
+
+
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0){
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+        $this->jmlHalaman = $jmlHalaman;
+        $this->waktuMain = $waktuMain;
+    }
+
+    public function getLabel(){
+        return "$this->penulis,$this->penerbit";
+    }
+
+    public function getInfoProduk(){
+        $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+        
+        if ($this->tipe == "Komik") {
+            $str .= " - {$this->jmlHalaman} Halaman.";
+        } else if($this->tipe == "Game"){
+            $str .= " - {$this->waktuMain} Jam.";
+        }
+
+        return $str;
+        
+    }
+
+}
+
+class Komik extends Produk{
+    public function getInfoProduk(){
+        $str = "Komik :" . parent::getInfoProduk() . "- {$this->jmlHalaman} Halaman.";
+        return $str;
+    }
+}
+
+class Game extends Produk{
+    public function getInfoProduk(){
+        $str = "Game : " . parent::getInfoProduk() . " - {$this->waktuMain} Jam.";
+        return $str;
+    }
+}
+
+class cetakInfoProduk {
+    public function cetak( Produk $produk ){
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100,0);
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000,0,50);
+
+echo $produk1->getInfoProduk();
+echo "<br>";
+echo $produk2->getInfoProduk();
